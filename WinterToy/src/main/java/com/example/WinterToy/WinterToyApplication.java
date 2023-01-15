ac9c831e9255c23f11ccb0532d8249c47eee60b4
gu <82009882+kugu98@users.noreply.github.com>
2023-01-15 19:32:07 +0900

    초기 Setting

---
+++ WinterToy/src/main/java/com/example/WinterToy/WinterToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.WinterToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterToyApplication.class, args);
+	}
+
+}
---
+++ WinterToy/src/test/java/com/example/WinterToy/WinterToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WinterToy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
