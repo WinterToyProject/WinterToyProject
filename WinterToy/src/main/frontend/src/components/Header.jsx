a6b81f2c3cb7c4f1d5c912f94dbebbf3ef336d09
98__dj <kimdongju666@naver.com>
2023-01-17 22:50:17 +0900

    front sign_up

---
+++ WinterToy/src/main/frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="inner">
+        <Link className='site-name' to='/'>
+        <h3> 방방곡곡 </h3>
+        </Link>
+        <ul class="menu">
+          <li>
+            <Link className='header-nav-item' to='/signin'>
+              회원 가입
+            </Link>
+          </li>
+          <li>
+            <Link className='header-nav-item' to='/login'>
+              로그인
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
---
+++ WinterToy/src/main/frontend/src/pages/Signin.jsx
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from 'react';
+import './Signin.css';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+
+function Signin() {
+    const [name, setName] = useState('');
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const  {replace} = useNavigate();
+
+
+    const [nameValid, setNameValid] = useState(false)
+    const [idValid, setIdValid] = useState(false)
+    const [pwValid, setPwValid] = useState(false)
+    const [notAllow, setNotAllow] = useState(true)
+
+    const register = () => {
+        axios
+    .post('http://localhost:8080/user/signup', {
+        username: name,
+        id: id,
+        password: pw,
+    })
+    .then(response => {
+    // Handle success.
+        console.log('Well done!');
+        console.log('User profile', response.data.user);
+        console.log('User token', response.data.jwt);
+        localStorage.setItem('token', response.data.jwt);
+        replace("/");
+    })
+    .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+    });
+    }
+
+
+    const handleName = (e) => {
+        setName(e.target.value);
+        const regex =
+        /^[가-힣]{2,5}$/;
+        if (regex.test(name)) {
+            setNameValid(true);
+        } else {
+            setNameValid(false);
+        }
+    }
+    const handleId = (e) => {
+        setId(e.target.value);
+        const regex = 
+        /^[a-z0-9_]{4,12}$/;
+        if (regex.test(id)) {
+            setIdValid(true);
+        } else {
+            setIdValid(false);
+        }
+    }
+    const handlePw = (e) => {
+        setPw(e.target.value);
+        const regex = 
+        /^(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]|.*[0-9]).{8,24}$/;
+        if (regex.test(pw)) {
+            setPwValid(true);
+        } else {
+            setPwValid(false);
+        }
+    }
+
+    useEffect(() => {
+        if(nameValid && idValid && pwValid) {
+            setNotAllow(false);
+            return;
+        }
+        setNotAllow(true);
+    }, [nameValid , idValid , pwValid]);
+    
+
+    
+    
+
+    return (
+        <div className='page'>
+            <div className="titleWrap">
+                회원 가입
+            </div>
+
+            <div className="contentWrap">
+                <div className="inputTitle">이름</div>
+                <div className="inputWrap">
+                    <input 
+                        type='text'
+                        className="input" 
+                        placeholder='홍길동'
+                        value={name}
+                        onChange={handleName}
+                        />
+                </div>
+                <div className="errorMessage">
+                    {
+                        !nameValid && name.length > 0 && (
+                            <div>올바른 이름을 입력해주세요.</div>
+                        )
+                    }
+                </div>
+
+                <div style={{ marginTop: "26px"}} className="inputTitle">아이디</div>
+                <div className="inputWrap">
+                    <input 
+                        type='text'
+                        className="input"
+                        placeholder='4~12자 영문소문자, 숫자 입력'
+                        value={id}
+                        onChange={handleId}
+                        />
+                </div>
+                <div className="errorMessage">
+                {
+                        !idValid && id.length > 0 && (
+                            <div>올바른 아이디를 입력해주세요.</div>
+                        )
+                    }
+                </div>
+
+                <div style={{ marginTop: "26px"}} className="inputTitle">비밀번호</div>
+                <div className="inputWrap">
+                    <input 
+                        type='password'
+                        className="input"
+                        placeholder='영문대소문자, 숫자, 특수문자 포함 8자 이상 입력'
+                        value={pw}
+                        onChange={handlePw} 
+                        />
+                </div>
+                <div className="errorMessage">
+                {
+                        !pwValid && pw.length > 0 && (
+                            <div>영문대소문자, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+                        )
+                }
+                </div>
+            </div>
+
+            <div>
+                <button disabled={notAllow} className='bottomButton' onClick={() => {
+                    register();
+                }}>
+                    확인
+                </button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Signin;
