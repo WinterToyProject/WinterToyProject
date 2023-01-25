bbb8e8931dfde6048867d5f3c89171777af375c5
gu <82009882+kugu98@users.noreply.github.com>
2023-01-25 15:41:38 +0900

    비밀번호 암호화

--- WinterToy/src/main/java/com/example/WinterToy/login/service/UserService.java
+++ WinterToy/src/main/java/com/example/WinterToy/login/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.WinterToy.login.data.repository.UserRepository;
 import com.example.WinterToy.login.data.repository.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,12 +15,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public final UserRepository userRepository;
     public String signup(UserDto request){
 
         userRepository.save(User.builder()
                 .userId(request.getUserId())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .userName(request.getUserName())
                 .build());
         return "Success";
