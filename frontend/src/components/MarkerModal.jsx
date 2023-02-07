3a0c2ebe0294658fa8bc2b0ffd5edc9a279b299f
98__dj <kimdongju666@naver.com>
2023-02-07 17:01:26 +0900

    권태기 해결...?

--- frontend/src/components/MarkerModal.jsx
+++ frontend/src/components/MarkerModal.jsx
@@ -1,69 +1,69 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import TextField from '@material-ui/core/TextField';
-import CloseIcon from '@material-ui/icons/Close';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import {actionCreators as markerActions} from '../redux/modules/marker';
+import React, { useState } from "react";
+import styled from "styled-components";
+import TextField from "@material-ui/core/TextField";
+import CloseIcon from "@material-ui/icons/Close";
+import axios from "axios";
 
-const MarkerModal =(props) => {
-  const dispatch = useDispatch()
-
+const MarkerModal = (props) => {
   // 사용자가 지정한 마커 이름을 담아냅니다.
-  const [ title, setTitle ] = useState()
-
+  const [text, setText] = useState();
 
   const selectTitle = (e) => {
-    console.log(e.target.value)
-    setTitle(e.target.value)
-  }
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
 
   // 마커를 생성할 때 사용되는 함수입니다.
-  const addMarker = () => {
-    let marker = {
-      // Map.js에서 받아온 값도 있고 여기서 생성한 값도 있습니다.
-      latitude: props.latitude,
-      longitude : props.longitude,
-      title: title,
-    }
+  const addMarker = (e) => {
 
-    /*axios.post('http://localhost:8080/gps/save',
+    axios.post('http://localhost:8080/gps/save',
     {
       latitude: props.latitude,
       longitude: props.longitude,
-      title: title,
+      title: text,
     })
     .then((response) => {
       console.log(response.data)
-    })*/
+    })
+    .catch(function (error) {
+      alert('실패했습니다!');
+    });
+    /*let marker = {
+      // Map.js에서 받아온 값도 있고 여기서 생성한 값도 있습니다.
+      latitude: props.latitude,
+      longitude: props.longitude,
+      title: title,
+    };*/
 
-    // 새 마커 값을 담아서 미들웨어 함수를 실행시킵니다.
-    dispatch(markerActions.addMarkerAX(marker))
+    
 
     // 마커 생성 모달 창을 닫습니다.
-    props.close()
-  }
+    props.close();
+  };
 
-  return(
+  return (
     <React.Fragment>
       <Component onClick={props.close} />
       <ModalComponent>
-        <ModalExitBtn onClick={props.close} >
-          <CloseIcon/>
+        <ModalExitBtn onClick={props.close}>
+          <CloseIcon />
         </ModalExitBtn>
         <ModalHeader>마커를 생성하시겠습니까?</ModalHeader>
         <ModalInput>
-          <TextField id="standard-basic" label="장소를 입력해주세요." style={{width: '100%'}} onChange={selectTitle} />
+          <TextField
+            id="standard-basic"
+            label="장소를 입력해주세요."
+            style={{ width: "100%" }}
+            onChange={selectTitle}
+          />
         </ModalInput>
         <ModalButtonContainer>
-          <ModalSubmitBtn onClick={addMarker}>
-            마커 생성
-          </ModalSubmitBtn>
+          <ModalSubmitBtn onClick={addMarker}>마커 생성</ModalSubmitBtn>
         </ModalButtonContainer>
       </ModalComponent>
     </React.Fragment>
-  )
-}
+  );
+};
 
 const Component = styled.div`
   position: fixed;
@@ -73,7 +73,7 @@ const Component = styled.div`
   width: 100vw;
   background-color: black;
   z-index: 10;
-`
+`;
 
 const ModalComponent = styled.div`
   position: fixed;
@@ -88,26 +88,26 @@ const ModalComponent = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`
+`;
 
 const ModalHeader = styled.div`
   margin-top: 30px;
   font-weight: 600;
   font-size: 18px;
-`
+`;
 
 const ModalInput = styled.div`
-  box-sizing: border-box; 
+  box-sizing: border-box;
   width: 50%;
-`
+`;
 const ModalButtonContainer = styled.div`
-  box-sizing: border-box; 
+  box-sizing: border-box;
   width: 50%;
   margin-bottom: 30px;
-`
+`;
 const ModalSubmitBtn = styled.button`
   width: 100%;
-  background-color: #FFE812;
+  background-color: #ffe812;
   border: none;
   outline: none;
   padding: 10px 0;
@@ -118,7 +118,7 @@ const ModalSubmitBtn = styled.button`
   &:hover {
     opacity: 0.7;
   }
-`
+`;
 const ModalExitBtn = styled.button`
   position: absolute;
   top: 0;
@@ -129,6 +129,6 @@ const ModalExitBtn = styled.button`
   outline: none;
   border: none;
   color: black;
-`
+`;
 
-export default MarkerModal
+export default MarkerModal;
