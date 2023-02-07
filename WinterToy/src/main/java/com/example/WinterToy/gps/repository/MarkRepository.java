487d4fc75e7b5f1e18281d57e8a4b21923b3a6d3
gu <82009882+kugu98@users.noreply.github.com>
2023-02-07 17:41:10 +0900

    List 반환

--- WinterToy/src/main/java/com/example/WinterToy/gps/repository/MarkRepository.java
+++ WinterToy/src/main/java/com/example/WinterToy/gps/repository/MarkRepository.java
@@ -4,7 +4,6 @@ import com.example.WinterToy.gps.repository.entity.Mark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MarkRepository extends JpaRepository<Mark, String> {
         List<Mark> findByUserId(String userId);
